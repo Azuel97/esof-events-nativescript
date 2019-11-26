6ca92cc12d0a518034491e23a54b4decfea7dd8b
Utente <utente@MacBook-Pro-di-Utente.local>
2019-11-26 17:54:25 +0100

    add plugin RadListView

---
+++ app/schedule/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+          {
+            "nome": "Colazione",
+            "inizio": "8:00",
+            "fine": "9:00",
+            "luogo": "Caffè degli specchi",
+            "id": "162gs38dh",
+            "group":"8:00 - 9:00"
+          },
+          {
+            "nome": "Presentazione ESOF 2019",
+            "inizio": "8:00",
+            "fine": "9:00",
+            "luogo": "Piazza Unità d'Italia",
+            "id": "@263gs79",
+            "group":"8:00 - 9:00"
+          },
+          {
+            "nome": "FINE",
+            "inizio": "10:00",
+            "fine": "11:00",
+            "luogo": "Piazza Unità d'Italia",
+            "id": "@263gs79",
+            "group":"10:00 - 11:00"
+          }
+        ]
--- app/schedule/schedule-items-view-model.js
+++ app/schedule/schedule-items-view-model.js
@@ -1,73 +1,18 @@
 const observableModule = require("tns-core-modules/data/observable");
+const data = require("./data")
+
+const redata = data.map((item) => {
+    item.sub = `${item.inizio} - ${item.fine} ${item.luogo}`
+    return item
+});
+
 
 function ScheduleItemsViewModel() {
     const viewModel = observableModule.fromObject({
-        items: [
-            {
-                name: "Colazione",
-                description: "Schedule for Item 1"
-            },
-            {
-                name: "Schedule 2",
-                description: "Schedule for Item 2"
-            },
-            {
-                name: "Schedule 3",
-                description: "Schedule for Item 3"
-            },
-            {
-                name: "Schedule 4",
-                description: "Schedule for Item 4"
-            },
-            {
-                name: "Schedule 5",
-                description: "Schedule for Item 5"
-            },
-            {
-                name: "Schedule 6",
-                description: "Schedule for Item 6"
-            },
-            {
-                name: "Schedule 7",
-                description: "Schedule for Item 7"
-            },
-            {
-                name: "Schedule 8",
-                description: "Schedule for Item 8"
-            },
-            {
-                name: "Schedule 9",
-                description: "Schedule for Item 9"
-            },
-            {
-                name: "Schedule 10",
-                description: "Schedule for Item 10"
-            },
-            {
-                name: "Schedule 11",
-                description: "Schedule for Item 11"
-            },
-            {
-                name: "Schedule 12",
-                description: "Schedule for Item 12"
-            },
-            {
-                name: "Schedule 13",
-                description: "Schedule for Item 13"
-            },
-            {
-                name: "Schedule 14",
-                description: "Schedule for Item 14"
-            },
-            {
-                name: "Schedule 15",
-                description: "Schedule for Item 15"
-            },
-            {
-                name: "Schedule 16",
-                description: "Schedule for Item 16"
-            }
-        ]
+        items: redata,
+        myGroupingFunc: function(item) {
+            return item.group
+        }
     });
 
     return viewModel;
