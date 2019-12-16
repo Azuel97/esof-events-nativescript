b673509b7700416688ccb4d1eb92cec1eaecac8d
Utente <utente@MacBook-Pro-di-Utente.local>
2019-12-16 15:59:53 +0100

    remove google api key

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 const application = require("tns-core-modules/application");
+const config = require("./config");
+const MapAPI = config.google.map.provideAPIKey || "AIzaSyCuaTZ-vUIU76Yfw032umnabe_cp1y-EZc";
 
 // Google Maps SDK - API KEY
 if(application.ios){
-    GMSServices.provideAPIKey("AIzaSyCuaTZ-vUIU76Yfw032umnabe_cp1y-EZc");
+    GMSServices.provideAPIKey(MapAPI);
 }
 
 application.run({ moduleName: "app-root" });
